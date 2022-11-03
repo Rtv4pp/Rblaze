37f9fd3d325fc5bb026bcca9eb0d96dfbeaccec2
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-11-02 22:49:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import json
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style, init
 
-from webdriver_manager.chrome import ChromeDriverManager
-
 chrome_options = Options()
 chrome_options.add_argument("-headless")
 nav = webdriver.Chrome(options = chrome_options)
@@ -24,104 +22,119 @@ bot = telegram.Bot(token)
 nav.get('https://blaze.com/pt/games/double')
 
 foradogiro = 0
+semutilidade = 0
 
 
+os.system('cls') or None
+print(Fore.GREEN + 'BOT INICIADO!')
+print(Style.RESET_ALL)
 
 while True:
 
     try:
 
-	    resulROOL = nav.find_element(By.XPATH, '//*[@id="roulette-timer"]/div[1]').text
-	    url = 'https://blaze.com/api/roulette_games/recent'
+        resulROOL = nav.find_element(By.XPATH, '//*[@id="roulette-timer"]/div[1]').text
 
-	    response = requests.get(url)
+        url = 'https://blaze.com/api/roulette_games/recent'
 
-	    r = response.json()
+        response = requests.get(url)
 
-	    ray = []
+        r = response.json()
 
-	    for x in range(len(r)):
-	        val = r[x]['color']
-	        if val == 1:
-	            val = 'V'
+        ray = []
 
-	        if val == 2:
-	            val = 'P'
+        for x in range(len(r)):
+            val = r[x]['color']
+            if val == 1:
+                val = 'V'
 
-	        if val == 0:
-	            val = 'B'
+            if val == 2:
+                val = 'P'
 
-	        ray.append(val)
+            if val == 0:
+                val = 'B'
+
+            ray.append(val)
+
+
+
+
+
+        if resulROOL == 'Girando...':
+            foradogiro = 1
+
+        if foradogiro == 1 and resulROOL != 'Girando...':
+
+            def resultado(num):
+
+    #==========================================Estrategia Sequencia============================#
+
+                if num[0:3] == ['V', 'P', 'P']:
+
+                    if num[0:4] == ['V', 'P', 'P', 'P']:
+                        return
+
+                    elif num[0:3] == ['V', 'P', 'P']:
+                        msg = '''❌LOSS'''
+                        mensagem = bot.send_message(chat_id=chat_id, text=msg)
+                        return
+
+
+                elif num[0:3] == ['P', 'P', 'P']:
+
+                    if num[0:4] == ['P', 'P', 'P', 'P']:
+                        return
+
+                    elif num[0:3] == ['P', 'P', 'P']:
+                        msg = '''✅ GREEN no ⚫🍷🗿'''
+                        mensagem = bot.send_message(chat_id=chat_id, text=msg)
+                        return
+                    elif num[0:3] == ['B', 'P', 'P']:
+                        msg = '''✅ GREEN no ⚪🍷🗿'''
+                        mensagem = bot.send_message(chat_id=chat_id, text=msg)
+                        return
+
+                elif num[0:2] == ['P', 'P']:
+
+                    if num[0:3] == ['P', 'P', 'P']:
+                        return
+
+                    elif num[0:2] == ['P', 'P']:
+                        msg = '''🚨Entrada confirmada🚨
+📌Entrar no ⚫ 
+🛡️Proteger o ⚪'''
+                        mensagem = bot.send_message(chat_id=chat_id, text=msg)
+                        return
+
+
+                elif num[0:1] == ['P']:
+
+                    if num[0:2] == ['P', 'P']:
+                        return
+
+                    elif num[0:1] == ['P']:
+                        msg = '''⚠️ATENÇÃO⚠️
+Possivel entrada no ⚫ 
+⏰Aguarde o sinal...'''
+                        mensagem = bot.send_message(chat_id=chat_id, text=msg)
+                        return
+
+    #==========================================FIM========================================#
+
+            foradogiro = 0
+            resultado(ray)
+            print(ray)
+            print(Fore.YELLOW + 'FIM DA RODADA!')
+            print(Style.RESET_ALL)
+
+
+
+
+
+
+
 
     except NameError as erro:
-        print('Ocorreu um erro de codigo "403" mas o bot está reiniciando!')
+        semutilidade = 1
     except Exception as erro:
-        print('Ocorreu um erro de codigo "404" mas o bot está reiniciando!')
-
-    if resulROOL == 'Girando...':
-        foradogiro = 1
-
-
-
-    if foradogiro == 1 and resulROOL != 'Girando...':
-
-	    def resultado(num):
-
-	        if num[0:4] == ['P', 'V', 'V', 'V']:
-
-	            msg = '''✅ GREEN no ⚫'''
-	            mensagem = bot.send_message(chat_id=chat_id, text=msg)
-
-	        elif num[0:4] == ['V', 'V', 'V', 'V']:
-
-	            text = '''✅ LOSS'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-	        elif num[0:3] == ['V', 'V', 'V']:
-
-	            text = '''✅ Entrada confirmada, entrar no ⚫
-	                      Buscar apoio no ⚪'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-	        elif num[0:2] == ['V', 'V']:
-
-	            text = '''✅ Possivel entrada no ⚫
-	                Buscar apoio no ⚪'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-
-
-
-
-
-	        elif num[0:4] == ['V', 'P', 'P', 'P']:
-
-	            text = '''✅ GREEN no 🔴'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-	        elif num[0:4] == ['P', 'P', 'P', 'P']:
-
-	            text = '''✅ LOSS'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-	        elif num[0:3] == ['P', 'P', 'P']:
-
-	            text = '''✅ Entrada confirmada, entrar no 🔴
-	                   Buscar apoio no ⚪'''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-
-	        elif num[0:2] == ['P', 'P']:
-
-	            text = '''✅ Possivel entrada no 🔴
-	                 Buscar apoio no ⚪ '''
-	            url_base = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-	            results = requests.get(url_base)
-	        print(num)
-	    foradogiro = 0
-	    resultado(ray)
+        semutilidade = 0
