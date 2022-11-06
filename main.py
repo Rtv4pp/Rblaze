90028b0600f7af6bceb2bcfae1ae524da5f94e15
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-11-06 03:07:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ import re
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style, init
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')  
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--window-size=1920,1080')
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')  
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1080')
 
 token = '5744690430:AAHdhSKGoDml-c-6jDoAXsTZrZ7py-uVryU'
 chat_id = '-1001896645285'
 bot = telegram.Bot(token)
 
 
-nav = webdriver.Chrome(options = chrome_options)
+nav = webdriver.Chrome(options = options)
 
-pegaporcentagem = webdriver.Chrome(options = chrome_options)
+pegaporcentagem = webdriver.Chrome(options = options)
 
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-nav = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-pegaporcentagem = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+nav = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+pegaporcentagem = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
 nav.get('https://blaze.com/pt/games/double')
 pegaporcentagem.get('https://tipminer.com/blaze/double')
