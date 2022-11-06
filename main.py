44c9c16ac767220f53a6e29c384efd00c1a21490
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-11-06 03:26:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import os
 import colorama
 import json
